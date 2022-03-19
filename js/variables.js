2df9a20e36ddb3443d4cc341ca6151921ff911d1
martuche <97261649+martuche@users.noreply.github.com>
2022-03-19 18:49:33 -0300

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -17,8 +17,9 @@ const listadoCarrito = document.getElementById("listadoCarrito")
 
 //GENERO NUEVOS NODOS EN PRESENTACION Y DEFINO SU CLASE
 const presentacion = document.getElementById("presentacion")
-presentacion.innerHTML = "<p class='col-md-6'>ACA VA UNA PRESENTACION</p><ul class='col-md-6'><li>Lista1</li><li>Lista2</li></ul>"
 presentacion.className = "row"
+presentacion.innerHTML = "<p class='col-md-6'>ACA VA UNA PRESENTACION</p><ul class='col-md-6'><li>Lista1</li><li>Lista2</li></ul>"
+
 
 // //IMAGENES DE INICIO
 // const imgInicio = document.getElementsByClassName("imgInicio")
